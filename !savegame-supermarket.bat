5a6bc6ae11b9d6f65dd6006fba57a926f11f05a9
GamerHD1991 <64063034+GamerHD1991@users.noreply.github.com>
2024-04-09 03:40:12 +0200

    Create !savegame-supermarket.bat

---
+++ !savegame-supermarket.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+REM Schritt 1: Löschen alter Daten im Ordner, außer bestimmten Dateien und Ordnern
+echo Löschen alter Daten im Ordner...
+REM Fügen Sie hier den Code zum Löschen alter Daten ein.
+
+REM Schritt 2: Löschen bestimmter einzelner Dateien
+echo Löschen bestimmter einzelner Dateien...
+REM Fügen Sie hier den Code zum Löschen bestimmter einzelner Dateien ein.
+
+REM Schritt 3: Löschen der übrigen Dateien und Ordner im aktuellen Verzeichnis
+echo Löschen übriger Dateien und Ordner im aktuellen Verzeichnis...
+REM Fügen Sie hier den Code zum Löschen der übrigen Dateien und Ordner ein.
+
+REM Schritt 4: Sprachausgabe mit angepasster Lautstärke
+echo Hallo Supermarkt-Fan! In diesen Ordner Befinden sich alle Safe-Game-Dateien. Ein grosses Dankeschon geht an Gamer-HD-1991! Bitte lasse ihn es wissen und folge ihm auf twitch und mehr. | powershell -command "& {Add-Type -AssemblyName System.Speech; $synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer; $synthesizer.Volume = 50; $synthesizer.Speak([Console]::In.ReadToEnd())}"
+
+REM Warten, bis die Sprachausgabe abgeschlossen ist (5 Sekunden)
+timeout /t 1 /nobreak >nul
+
+REM Öffnen der Twitch-Seite von Gamer HD 1991
+start https://www.twitch.tv/gamerhd1991
+
+start "" "%AppData%\..\LocalLow\Nokta Games\Supermarket Simulator"
+
+exit
